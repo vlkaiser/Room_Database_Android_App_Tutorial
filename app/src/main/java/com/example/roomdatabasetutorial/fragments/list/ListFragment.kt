0e7c856989b80fbdc176bf202622389aaabde0ea
vlkaiser <vlkaiser9@gmail.com>
2021-09-20 15:30:08 -0400

    Updated: Reorganized fragments and refactored

--- app/src/main/java/com/example/roomdatabasetutorial/fragments/list/ListFragment.kt
+++ app/src/main/java/com/example/roomdatabasetutorial/fragments/list/ListFragment.kt
@@ -1,19 +1,22 @@
-package com.example.roomdatabasetutorial
+package com.example.roomdatabasetutorial.fragments.list
 
 import android.os.Bundle
 import androidx.fragment.app.Fragment
 import android.view.LayoutInflater
 import android.view.View
 import android.view.ViewGroup
+import com.example.roomdatabasetutorial.R
 
-class AddFragment : Fragment() {
+
+class ListFragment : Fragment() {
 
     override fun onCreateView(
         inflater: LayoutInflater, container: ViewGroup?,
         savedInstanceState: Bundle?
     ): View? {
         // Inflate the layout for this fragment
-        return inflater.inflate(R.layout.fragment_add, container, false)
+        return inflater.inflate(R.layout.fragment_list, container, false)
     }
+
 
 }
