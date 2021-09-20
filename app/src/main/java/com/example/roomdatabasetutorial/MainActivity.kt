882109f9e7ec59647ef637a418a3c0222315b3f3
vlkaiser <vlkaiser9@gmail.com>
2021-09-20 16:58:11 -0400

    Added back button to FragmentAdd

--- app/src/main/java/com/example/roomdatabasetutorial/MainActivity.kt
+++ app/src/main/java/com/example/roomdatabasetutorial/MainActivity.kt
@@ -2,10 +2,27 @@ package com.example.roomdatabasetutorial
 
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
+import androidx.navigation.findNavController
+import androidx.navigation.fragment.NavHostFragment
+import androidx.navigation.ui.setupActionBarWithNavController
 
-class MainActivity : AppCompatActivity() {
+class MainActivity() : AppCompatActivity() {
+
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_main)
+
+
+        val navHostFragment = supportFragmentManager
+            .findFragmentById(R.id.fragmentContainerView) as NavHostFragment
+        val navController = navHostFragment.navController
+        setupActionBarWithNavController(navController)
     }
+
+    //Use the navigate back arrow on Entry fragment
+    override fun onSupportNavigateUp(): Boolean {
+        val navController = findNavController(R.id.fragmentContainerView)
+        return navController.navigateUp() || super.onSupportNavigateUp()
+    }
+
 }
