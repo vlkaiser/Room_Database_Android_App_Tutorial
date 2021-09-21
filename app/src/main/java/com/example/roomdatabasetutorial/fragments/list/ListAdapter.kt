0e9a3d30626b3e2b11185938bb6e7a127671e82b
vlkaiser <vlkaiser9@gmail.com>
2021-09-21 10:19:44 -0400

    Updated ListaAdapter class to bind viewholder to customrow layout

--- app/src/main/java/com/example/roomdatabasetutorial/fragments/list/ListAdapter.kt
+++ app/src/main/java/com/example/roomdatabasetutorial/fragments/list/ListAdapter.kt
@@ -1,11 +1,41 @@
 package com.example.roomdatabasetutorial.fragments.list
 
+import android.view.LayoutInflater
 import android.view.View
+import android.view.ViewGroup
 import androidx.recyclerview.widget.RecyclerView
+import com.example.roomdatabasetutorial.R
+import com.example.roomdatabasetutorial.data.User
+import kotlinx.android.synthetic.main.custom_row.view.*
 
 class ListAdapter:RecyclerView.Adapter<ListAdapter.MyViewHolder>() {
+
+    private var userList = emptyList<User>()
 
     class MyViewHolder(itemView: View): RecyclerView.ViewHolder(itemView) {
 
     }
+
+    override fun onCreateViewHolder(parent: ViewGroup, viewType: Int): MyViewHolder {
+        return MyViewHolder(LayoutInflater.from(parent.context).inflate(R.layout.custom_row, parent, false))
+    }
+
+    override fun getItemCount(): Int {
+        return userList.size
+    }
+
+    override fun onBindViewHolder(holder: MyViewHolder, position: Int) {
+        var currentItem = userList[position]
+        holder.itemView.id_txt.text = currentItem.id.toString()
+        holder.itemView.firstName_txt.text = currentItem.firstName
+        holder.itemView.lastName_txt.text = currentItem.lastName
+        holder.itemView.age_txt.text = currentItem.age.toString()
+    }
+
+    fun setData(user:List<User>){
+        this.userList = user
+        notifyDataSetChanged()
+    }
+
+
 }
