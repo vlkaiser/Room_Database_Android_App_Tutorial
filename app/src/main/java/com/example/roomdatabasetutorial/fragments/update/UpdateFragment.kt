622c93ae120e18447d15dc364645d962c3a22854
vlkaiser <vlkaiser9@gmail.com>
2021-09-21 16:59:48 -0400

    Updated: onclick update, database entries are populated in update fields for user to edit

--- app/src/main/java/com/example/roomdatabasetutorial/fragments/update/UpdateFragment.kt
+++ app/src/main/java/com/example/roomdatabasetutorial/fragments/update/UpdateFragment.kt
@@ -5,17 +5,26 @@ import androidx.fragment.app.Fragment
 import android.view.LayoutInflater
 import android.view.View
 import android.view.ViewGroup
+import androidx.navigation.fragment.navArgs
 import com.example.roomdatabasetutorial.R
+import kotlinx.android.synthetic.main.fragment_update.view.*
 
 class UpdateFragment : Fragment() {
 
+    private val args by navArgs<UpdateFragmentArgs>()
 
     override fun onCreateView(
         inflater: LayoutInflater, container: ViewGroup?,
         savedInstanceState: Bundle?
     ): View? {
         // Inflate the layout for this fragment
-        return inflater.inflate(R.layout.fragment_update, container, false)
+        val view =  inflater.inflate(R.layout.fragment_update, container, false)
+
+        view.updateFirstName_et.setText(args.currentUser.firstName)
+        view.updateLastName_et.setText(args.currentUser.lastName)
+        view.updateAge_et.setText(args.currentUser.age.toString())
+
+        return view
     }
 
 
