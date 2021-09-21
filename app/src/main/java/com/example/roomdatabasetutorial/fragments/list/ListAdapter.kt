fdfb04a9ba4ddab84be5bc407b8a800b35a0fb09
vlkaiser <vlkaiser9@gmail.com>
2021-09-21 10:13:25 -0400

    Added: custom Row layout for database layout.  Added: Listadapter class to link recyclerview to database layout

---
+++ app/src/main/java/com/example/roomdatabasetutorial/fragments/list/ListAdapter.kt
@@ -0,0 +1,11 @@
+package com.example.roomdatabasetutorial.fragments.list
+
+import android.view.View
+import androidx.recyclerview.widget.RecyclerView
+
+class ListAdapter:RecyclerView.Adapter<ListAdapter.MyViewHolder>() {
+
+    class MyViewHolder(itemView: View): RecyclerView.ViewHolder(itemView) {
+
+    }
+}
