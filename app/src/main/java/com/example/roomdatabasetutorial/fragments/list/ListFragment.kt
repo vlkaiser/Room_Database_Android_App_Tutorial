c6d6172442616e6fd62bed9cdb4067496684c7ac
vlkaiser <vlkaiser9@gmail.com>
2021-09-21 20:42:01 -0400

    New branch - Try to scale for tablet

--- app/src/main/java/com/example/roomdatabasetutorial/fragments/list/ListFragment.kt
+++ app/src/main/java/com/example/roomdatabasetutorial/fragments/list/ListFragment.kt
@@ -1,15 +1,17 @@
 package com.example.roomdatabasetutorial.fragments.list
 
+import android.app.AlertDialog
 import android.os.Bundle
+import android.view.*
 import androidx.fragment.app.Fragment
-import android.view.LayoutInflater
-import android.view.View
-import android.view.ViewGroup
+import android.widget.Toast
+import androidx.core.content.ContentProviderCompat.requireContext
 import androidx.lifecycle.Observer
 import androidx.lifecycle.ViewModelProvider
 import androidx.navigation.fragment.findNavController
 import androidx.recyclerview.widget.LinearLayoutManager
 import com.example.roomdatabasetutorial.R
+import com.example.roomdatabasetutorial.model.User
 import com.example.roomdatabasetutorial.viewmodel.UserViewModel
 import kotlinx.android.synthetic.main.fragment_list.view.*
 
@@ -37,12 +39,41 @@ class ListFragment : Fragment() {
             adapter.setData(user)
         })
 
-        view.floatingActionButton.setOnClickListener{
+        view.floatingActionButton.setOnClickListener {
             findNavController().navigate(R.id.action_listFragment_to_addFragment)
         }
+
+        setHasOptionsMenu(true)
 
         return view
     }
 
+    override fun onCreateOptionsMenu(menu: Menu, inflater: MenuInflater) {
+        inflater.inflate(R.menu.delete_menu, menu)
+    }
 
+    override fun onOptionsItemSelected(item: MenuItem): Boolean {
+        if (item.itemId == R.id.menu_delete) {
+            deleteAllUsers()
+
+        }
+        return super.onOptionsItemSelected(item)
+    }
+
+    private fun deleteAllUsers() {
+        val builder = AlertDialog.Builder(requireContext())
+        builder.setPositiveButton("Yes") { _, _ ->
+            mUserViewModel.deleteAllUsers()
+            Toast.makeText(
+                requireContext(),
+                "Successfully cleared Database",
+                Toast.LENGTH_LONG
+            ).show()
+        }
+        builder.setNegativeButton("No") { _, _ -> }
+        builder.setTitle("Delete Database")
+        builder.setMessage("Are you sure you want to clear this Database?")
+        builder.create().show()
+
+    }
 }
